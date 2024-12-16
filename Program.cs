51a3b717ad07ecb85914dbe1dcb519e703a3ee7d
kinich-web <ivanterzi96@gmail.com>
2024-12-17 01:53:54 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //        //Task 1
+            //        Console.Write("Введите номер дня недели (от 1 до 7): ");
+            //        int n = Convert.ToInt32(Console.ReadLine());
+
+
+            //switch (n)
+            //{
+            //  case 1:
+            //        Console.WriteLine("Понедельник");
+            //        break;
+            //  case 2:
+            //        Console.WriteLine("Вторник");
+            //        break;
+            //  case 3:
+            //        Console.WriteLine("Среда");
+            //        break;
+            //  case 4:
+            //        Console.WriteLine("Четверг");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Пятница");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Суббота");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Воскресенье");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Такого нема");
+            //        break;
+
+
+            //}
+            //Console.ReadKey();
+            //Task 2
+            //Console.WriteLine("Введите номер месяца (1-12):");
+            //int month = int.Parse(Console.ReadLine());
+            //switch (month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Январь");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Февраль");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Март");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Апрель");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Май");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Июнь");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Июль");
+            //        break;
+            //}
+            //Console.ReadKey();
+            //Task 3
+            //Console.WriteLine("Введите первую букву названия предмета:");
+            //char letter = char.ToLower(Console.ReadKey().KeyChar); 
+            //Console.WriteLine();
+            //switch (letter)
+            //{
+            //    case 'ф':
+            //        Console.WriteLine("Физика");
+            //        break;
+            //    case 'м':
+            //        Console.WriteLine("Математика");
+            //        break;
+            //    case 'и':
+            //        Console.WriteLine("История");
+            //        break;
+            //    case 'г':
+            //        Console.WriteLine("География");
+            //        break;
+            //    case 'б':
+            //        Console.WriteLine("Биология");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Такого нема!");
+            //        break;
+            //}
+            //Console.ReadKey();  
+            //Task 3
+            //    Console.WriteLine("Введите признак транспортного средства:");
+            //    char sign = char.ToLower(Console.ReadKey().KeyChar); 
+            //    Console.WriteLine(); 
+            //        switch (sign)
+            //        {
+            //            case 'a':
+            //                Console.WriteLine("Автомобиль: максимальная скорость - 180 км/ч");
+            //                break;
+            //            case 'в':
+            //                Console.WriteLine("Велосипед: максимальная скорость - 50 км/ч");
+            //                break;
+            //            case 'м':
+            //                Console.WriteLine("Мотоцикл: максимальная скорость - 220 км/ч");
+            //                break;
+            //            case 'с':
+            //                Console.WriteLine("Самолет: максимальная скорость - 900 км/ч");
+            //                break;
+            //            case 'п':
+            //                Console.WriteLine("Поезд: максимальная скорость - 200 км/ч");
+            //                break;
+            //            default:
+            //                Console.WriteLine("Нету такого :(");
+            //                break;
+            //        }
+            //    Console.ReadKey();
+            //}
+
+
+        }
+
+    }
+}
+
+
+
+
